be2054ceb5c1418f31d56de0712305dc15941f77
Emmanuel <eec.studies@gmail.com>
2021-04-05 18:10:55 +0100

    enable auto refresh

--- src/pages/homepage.component.jsx
+++ src/pages/homepage.component.jsx
@@ -1,9 +1,13 @@
+import { useEffect } from 'react';
 import Banner from '../components/banner.component';
 import AppDescription from '../components/app-description';
 import Header from '../components/header.component';
 import Footer from '../components/footer.component';
 
 const HomePage = () => {
+    useEffect(()=>{
+        window.sessionStorage.reload = "false";
+    })
     return (
         <div className="w-full flex flex-col items-center justify-center">
             <Header />
